115482f6d5d9162583c833a57cbf97cf8017d1c7
Yuri Marques dos Santos Rodrigues <yurimarques.el@gmail.com>
2023-05-14 20:07:10 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const BotaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+BotaoAlterarTema.addEventListener("click", () => {
+  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+  body.classList.toggle("modo-escuro");
+
+  if (modoEscuroEstaAtivo) {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+  } else {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+  }
+});
