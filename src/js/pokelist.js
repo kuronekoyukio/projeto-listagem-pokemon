e86317001e5c8dc7cf0b76c95670f841b8c2b9d9
Yuri Marques dos S. R <yurimarques.el@gmail.com>
2023-10-12 00:09:48 -0300

    esqueci

--- src/js/pokelist.js
+++ src/js/pokelist.js
@@ -5,6 +5,7 @@ const pokeList=[
     name:Bulbasaur,
     type1:plant,
     type2:null,
+    Image:,
     description:`á uma semente de planta nas costas desde o dia em que este Pokémon nasce. A semente cresce lentamente.`
   },];
 
